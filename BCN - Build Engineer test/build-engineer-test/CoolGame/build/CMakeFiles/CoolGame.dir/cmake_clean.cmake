1f5a39a4b823803b6bd2950d01da9b6508d1a541
David <davidsk2407@gmail.com>
2021-02-10 12:19:02 +0100

    first commit

---
+++ BCN - Build Engineer test/build-engineer-test/CoolGame/build/CMakeFiles/CoolGame.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CoolGame.dir/488fbc819a02c33030e1e12ad0302bd2/build-engineer-test/CoolGame/game/src/common/Game.cpp.obj"
+  "CMakeFiles/CoolGame.dir/488fbc819a02c33030e1e12ad0302bd2/build-engineer-test/CoolGame/game/src/common/GameObject.cpp.obj"
+  "CMakeFiles/CoolGame.dir/488fbc819a02c33030e1e12ad0302bd2/build-engineer-test/CoolGame/game/src/main.cpp.obj"
+  "CoolGame.exe"
+  "CoolGame.exe.manifest"
+  "CoolGame.pdb"
+  "libCoolGame.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CoolGame.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
